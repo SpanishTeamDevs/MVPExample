7401b594755495dd6eb0088456faff440cbf541d
Javier Arroyo Espallargas <jarroyoe@BCN-C02R24FEFVH8.local>
2016-06-30 17:21:08 +0200

    First commit

---
+++ app/src/main/java/example/mvp/mvpapplication/views/interfaces/MainViewOps.java
@@ -0,0 +1,10 @@
+package example.mvp.mvpapplication.views.interfaces;
+
+/**
+ * Created by jarroyoe on 30/6/16.
+ */
+public interface MainViewOps {
+    void showLoading(String text);
+    void hideLoading();
+    void navigate();
+}
